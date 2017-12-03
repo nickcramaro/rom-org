1fc7ca89aaa98e6431c7436a20b83f2bf46b5840
Nick Cramaro <nickcramaro@gmail.com>
2017-12-02 22:06:01 -0500

    Added temp directory and copy step

--- index.js
+++ index.js
@@ -2,32 +2,52 @@ const fs = require('fs');
 const zipLib = require('node-zip');
 
 const pathToRoms = `D:/games/roms/Super Nintendo Entertainment System Emulator & ROMs (844 Games)-KaN1vE/ROMS (Games)/SNES`;
-const inputPath = pathToRoms;
-const outputPath = `./output`;
+const pathToTest = `D:/dev/test`;
+const inputPath = pathToTest; // path to original files
+const outputPath = `D:/games/roms/snes`; // final path of files
+const tempPath = `./temp`; // temp working directory
 
 init();
 
 function init() {
-    renameRoms().then(() => {
-        ('Rename complete');
-        zipRoms().then(() => console.log('Process complete'));
+    copyToTemp().then(() => {
+        console.log('Copy complete');
+        renameRoms().then(() => {
+            console.log('Rename complete');
+            zipRoms().then(() => {
+                console.log('Zipping complete');
+            });
+        });
+    });
+}
+
+function copyToTemp() {
+    return readFilesInFolder(inputPath).then(files => {
+        console.log('Copying files...');
+        files.forEach(file => {
+            fs.copyFileSync(inputPath + '/' + file, tempPath + '/' + file);
+            console.log('Copied', file);
+        });
+
+        // resolve promise once loop is complete
+        return new Promise((resolve, reject) => {
+            
+            resolve();
+        });
     });
 }
 
 function renameRoms() {
-    return readFilesInFolder().then(files => {
-
+    return readFilesInFolder(tempPath).then(files => {
+        console.log('Renaming files...');
         files.forEach(file => {
-            console.log('Renaming files...');
             let newName = file
                 .replace(/\(U\)/, '') // strip (U)
                 .replace(/\[!\]/, '') // strip [!]
-                .replace(/(?<=\S)(.\s?)(?=\.zip)/, ''); // strip whitespace
+                .replace(/\S(\s*?).zip/, ''); // strip whitespace
 
-            // only attempt rename if change was made
-            if (file !== newName) {
-                fs.renameSync(inputPath + '/' + file, outputPath + '/' + newName + '.zip');
-            }
+            fs.renameSync(tempPath + '/' + file, outputPath + '/' + newName);
+            console.log('Renamed ', newName);
         });
 
         // resolve promise once loop is complete
@@ -39,18 +59,20 @@ function renameRoms() {
 }
 
 function zipRoms() {
-    return readFilesInFolder().then(files => {
+    return readFilesInFolder(outputPath).then(files => {
 
         files.forEach(file => {
-            ('Zipping files');
+            ('Zipping files...');
             if (!/.zip/.test(file)) {
                 let zip = new zipLib();
-                zip.file(file, fs.readFileSync(inputPath + '/' + file));
+                zip.file(file, fs.readFileSync(outputPath + '/' + file));
                 let data = zip.generate({ base64:false, compression: 'DEFLATE' });
                 fs.writeFileSync(outputPath + `/${file}.zip`, data, 'binary');
+                console.log('Zipped ', file);
             }
-        })
+        });
 
+        // resolve promise once loop is complete
         return new Promise((resolve, reject) => {
 
             resolve();
@@ -58,10 +80,10 @@ function zipRoms() {
     })
 }
 
-function readFilesInFolder() {
+function readFilesInFolder(pathToRead) {
     return new Promise((resolve, reject) => {    
         console.log('Reading files...');
-        fs.readdir(inputPath, (err, files) => {
+        fs.readdir(pathToRead, (err, files) => {
             if (err) reject(err);
             else resolve(files);
         });
